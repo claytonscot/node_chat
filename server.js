9427c77bf9207e0e1cfc20b87f26ad6a33845cb1
Clay Hougham <clayton.hougham@gmail.com>
2016-11-30 16:30:11 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+console.log("Worked");
+
+var mongo = require('mongodb').MongoClient,
+	client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+	if(err) throw err;
+	
+	client.on('connection', function(socket) {
+	
+	//collection of data
+	var col = db.collection('messages');
+	console.log("Someone has connected!!!");
+	
+		//wait for input
+		socket.on('input', function(data) {
+			var name = data.name,
+				message = data.message;
+				whitespacePattern = /^\s*$/;
+				
+				if(whitespacePattern.test(name) || whitespacePattern.test(message))
+				{
+					console.log("Invalid.");
+				}
+				else
+				{
+					//inserts data into collection
+				col.insert({name: name, message: message}, function() {
+					console.log("inserted");
+					
+					});
+				}	
+							
+		});
+	});
+});
+
